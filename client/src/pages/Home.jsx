64bbc56ba0c8eb57be3d92157a86ada8ef6efbcb
Aditya Chopde <its.adityac@gmail.com>
2025-02-21 15:51:01 +0530

    random commits

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      This is the home page
-    </div>
-  )
-}
+    <>
+      <p>Home</p>
+    </>
+  );
+};
 
-export default Home
+export default Home;
